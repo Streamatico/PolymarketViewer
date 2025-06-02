2e855eb4a13ff0011701cb2aa188b70a9e780efb
Streamatico <streamatico@gmail.com>
2025-06-02 20:08:58 +0300

    Release v1.11 - Reproducible Builds support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,15 +17,14 @@ android {
 
     defaultConfig {
         applicationId = "com.streamatico.polymarketviewer"
-        minSdk = 27
-        //noinspection OldTargetApi
+        minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.10"
+        versionCode = 2
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
-            useSupportLibrary = false
+            useSupportLibrary = true
         }
     }
 
