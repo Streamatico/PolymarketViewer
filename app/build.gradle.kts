272738b05c22f5cbdf8e2355bad60331a7e7c8b0
Streamatico <streamatico@gmail.com>
2025-10-25 01:45:49 +0300

    App v1.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 20
-        versionName = "1.20"
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
